6d3a742bf5e7d75ee94e80be29b22de81f061797
yunpoodog <144025622+yunpoodog@users.noreply.github.com>
2023-09-08 14:47:43 +0900

    Add Python_01_variable

---
+++ lecture/python_01_variable.py
@@ -0,0 +1,12 @@
+# 주석(comment): 메모, 실행x
+
+# 글씨체: D2Coding
+
+# 1.출력 함수 (print)
+# - ()안의 값을 출력
+# - 변수값 확인 용도로 많이 사용
+print("Hello Python")
+
+# 문자열 타입(String Type)
+# - Python: '' or "" -> String
+# - C, Java: ''(char), ""(String)
