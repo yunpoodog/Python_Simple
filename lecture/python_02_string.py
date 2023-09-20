edd6acc1034fae73e4b5b23eb7909530e17dd827
yunpoodog <144025622+yunpoodog@users.noreply.github.com>
2023-09-20 16:33:28 +0900

    Python_02_string

--- lecture/python_02_string.py
+++ lecture/python_02_string.py
@@ -38,3 +38,47 @@ print(msg[:])   # 처음부터 끝까지
 print(msg[18:])
 # 역방향
 print(msg[-4:])
+
+# 4. 문자열 함수
+str = "Hello World"
+
+print("=" * 200)
+# 4-1. len(): 문자열 길이 계산
+print(len(str))
+
+# 4-2. upper() and lower(): 대소문자 변경
+#   - 데이터 전처리 -> 1A, 1a -> upper() 1A 통일
+print(str.upper())
+print(str.lower())
+
+# 4-3. replace(): 문자열 내외 특정 문자 치환
+print(str.replace("H", "J"))
+
+# 4-4. split(): 구분자를 기준으로 문자열 분활(Default: 공백)
+b = "hello world what a nice weather"
+print(b.split) # 거의 이렇게 씀
+print(b.split("w")) # 이렇게 잘 안씀
+
+# 4-5. strip(): 문자열의 좌우공백 제거
+id = "             python1004             "
+print(id)
+print(id.strip())
+
+id = "           PyThon1004              "
+id.lower() #"          python1004                   "
+print(id.lower().strip())   # "python1004" - 여기서 .(~에서)은 참조연산자
+
+# 4-6. find() and rfind(): 문자열 내부의 특정 문자 위치 인덱스 출력
+print(str.find("o"))    # Hello^o^ World
+print(str.rfind("o"))   # Hello W^o^rld
+print(str.find("world"))    # 못찾으면 -1 출력
+print(str.find("World"))    # 단어의 첫 글자 인덱스
+print(str.rfind("World"))   # 단어의 첫 글자 인덱스
+
+# 4-7. in(): 특정 문자열 포함하는지 확인(Truem False 출력)
+print("Hi" in "Hi Python")
+
+# Quiz
+id = "cherry1004@gmail.com"
+
+print(id[0:10]) #cherry1004 출력
