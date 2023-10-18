6b801b9233c2513b18eb63fcb0932399168d6034
yunpoodog <144025622+yunpoodog@users.noreply.github.com>
2023-10-18 17:08:19 +0900

    project_kiosk
    finished kiosk

--- project_kiosk/service_kiosk.py
+++ project_kiosk/service_kiosk.py
@@ -12,3 +12,9 @@ def user_choice(max_cnt, menu_type = "sub"):
             return choice
         else:
             print("MSG: 올바른 번호를 입력하세요.")
+
+# 메뉴 출력 기능
+
+def show_menu(menu_list):
+    for i, menu in enumerate(menu_list.values()):
+        print(f"■□ {i+1}.{menu}")
